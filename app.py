d91cc706605116adc79d6637d68e18ef71864f22
aarizdeadshot-droid <aarizdeadshot@gmail.com>
2026-08-25 15:15:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,59 @@
-print("OP")
+print("a game developed by Aariz")
+print("welcome to person interview game")
+print("what is your name")
+name = input()
+greet=f"Hello {name}"
+print(greet)
+print("how old are you")
+oldAge=input()
+print(" Aariz : Good")
+print("whats your gender")
+gender=input()
+print(" Aariz : Okay")
+print("In which city do you live")
+city=input()
+print(" Aariz : That's a beautiful city")
+print("What is your favorite color?")
+color=input()
+print(" Aariz : okay")
+print("in which class do you study")
+className=input()
+print("here is a question for you")
+print("a=45")
+a=45
+print("b=50")
+b=50
+print("c=75")
+c=75
+print("d=75")
+d=75
+print("a+b+c-d+b-a?")
+number = int(input("Enter a number: "))
+# Start with an initial value that triggers the loop
+number = 0
+
+#Loop keeps running as long as the number is NOT 100
+while number != 100:
+    number = int(input("Enter a number: "))
+
+    if number != 100:
+        print("wrong number")
+    else:
+        print("correct")
+print(number)
+print("Aariz: 'Excellent answer!")
+print("now what is 9*6*89*78/45/34*34*76")
+number = int(input("Enter a number: "))
+# Start with an initial value that triggers the loop
+number = 0
+
+#Loop keeps running as long as the number is NOT 633,110.4
+while number != 633110.4:
+    number = int(input("Enter a number: "))
+
+    if number != 633110.4:
+        print("wrong number")
+    else:
+        print("correct")
+print(number)
+print("Aariz: 'Excellent answer!")
