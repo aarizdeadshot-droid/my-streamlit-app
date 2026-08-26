23956a7e94ddee94d7c6e8bceb6b77e8a28b7fdc
aarizdeadshot-droid <aarizdeadshot@gmail.com>
2026-08-26 13:37:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -19,3 +19,5 @@ if st.button("Submit Name"):
 st.write("where do you live")
 # Create an interactive text input box
 user_name = st.text_input("Enter your place:", placeholder="Type here...")
+# Create an interactive button
+if st.button("Submit Name"):
