6ef4e0ea1544eedb268e9e284a50a7cdb084ab11
aarizdeadshot-droid <aarizdeadshot@gmail.com>
2026-09-18 16:02:56 +0500

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # 1. Page Configuration
 st.set_page_config(
-    page_title="The Aariz Python Web",
+    page_title="The Aariz Bin Azmat Python Web",
     page_icon="✨",
     layout="centered",
 )
